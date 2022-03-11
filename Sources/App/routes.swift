fbcf1f2c6b3e6fbb88825fe1249a37b6f6395ddc
Misha Causur <mishacausur@mail.ru>
2022-03-12 01:45:20 +0500

    initial commit

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -4,11 +4,29 @@ import Vapor
 func routes(_ app: Application) throws {
     app.get { req in
         return req.view.render("index", ["title": "Hello Vapor!"])
-    }app.get { req in
-        return "It works!"
+    }; app.get { req in
+        return "NEW WEB APP WORKS!"
     }
 
     app.get("hello") { req -> String in
         return "Hello, world!"
     }
+    
+    app.get("main") { req -> String in
+        return "Main page"
+    }
+    
+    app.on(.GET, "one") { _ in
+        return "HIHIHIHIHI"
+    }
+    
+    app.post("post") { req -> String in
+        let post = try req.content.decode(Post.self)
+        print(post.name) // "world"
+        return "\(HTTPStatus.ok)"
+    }
+}
+
+struct Post: Content {
+    let name: String
 }
